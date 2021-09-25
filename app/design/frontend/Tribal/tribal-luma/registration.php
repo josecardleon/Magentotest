303c86ed168edf787d96e5bba9352e8f58755a1d
Jose Carlos de León <josecar_85@hotmail.com>
2021-09-25 00:53:13 -0600

    Final Test

--- app/design/frontend/Tribal/tribal-luma/registration.php
+++ app/design/frontend/Tribal/tribal-luma/registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Tribal/tribal_luma', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Tribal/tribal-luma', __DIR__);
